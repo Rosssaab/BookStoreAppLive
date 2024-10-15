5ff1789f11b491bcd9cabd175c8c1c19f22e97b7
Rosssaab <rosssaab@live.com>
2024-10-15 10:38:27 +0100

    Changed default port to 8085

--- app.py
+++ app.py
@@ -454,5 +454,5 @@ def add_country():
         conn.close()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 8085))
     app.run(host='0.0.0.0', port=port, debug=True)
